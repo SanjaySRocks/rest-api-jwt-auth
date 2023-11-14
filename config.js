62cd5f0519346ec25ce24ce0f293a10972951832
Sanjay Singh <30376256+SanjaySRocks@users.noreply.github.com>
2023-11-15 00:03:20 +0530

    add config variables

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js
+module.exports = {
+    MONGODB_URL: 'mongodb+srv://restapi:restapi@c1.i2rx4.mongodb.net/restapi?retryWrites=true&w=majority',
+    SECRET_KEY: 'thisissecretkey123123123'
+
+    // Add other environment variables here
+
+  };
+  
